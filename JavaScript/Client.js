26c2f1f74f0dba8ef0cc4019dfbb7baf1cd516c9
gardemarin565 <108128343+gardemarin565@users.noreply.github.com>
2022-08-31 22:41:15 +0300

    Update Client.js

--- JavaScript/Client.js
+++ JavaScript/Client.js
@@ -1,1 +1,18 @@
+const {IntentsBitField, Partials, Client} = require('discord.js'),
+    config = require('./config.json');
+config.cfg = {
+    ...config.cfg,
+    intents: new IntentsBitField(config.cfg.intents),
+    partials: [Partials.Channel]
+};
 
+    
+const bot = new Client(config.cfg);
+bot.login(config.token);
+
+const DiscordDB = require('simple-discord.db');
+bot.Memory = new DiscordDB("Memory", bot);
+bot.Memory.save();
+
+require('./handlers')(bot);
+require('./events')(bot);
